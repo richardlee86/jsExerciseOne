fea6b8624e4093469fe8e8894575f3604ef8e886
Richard Lee <richard.t.lee86@gmail.com>
2020-01-03 08:47:22 +0100

    two exercises completed (calculator and concantenation)

---
+++ calc.js
@@ -0,0 +1,3 @@
+var userAge = prompt("How old are you?");
+var ageInDays = parseInt(userAge) * 365;
+document.write("You are roughly " + ageInDays + " days old");
---
+++ script.js
@@ -0,0 +1,6 @@
+var firstName = prompt("Please enter your first name");
+var lastName = prompt("Please enter your last name");
+var age = prompt("Please enter your age");
+document.write("Hello " + firstName + " " + lastName + "! " + "I hear that you are " +  age + " years old!")
+
+console.log(firstName + " " + lastName + " " + "age: " + age);
